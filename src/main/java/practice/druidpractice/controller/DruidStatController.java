bdacb124d01e6f19b0180008eaa574a36f3d87b6
junyangwei <799004508@qq.com>
2021-11-08 23:14:03 +0800

    feat: 添加 Druid 及 DruiDataSource 属性配置

---
+++ src/main/java/practice/druidpractice/controller/DruidStatController.java
@@ -0,0 +1,20 @@
+package practice.druidpractice.controller;
+
+import com.alibaba.druid.stat.DruidStatManagerFacade;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author junyangwei
+ * @date 2021-11-08
+ */
+@RestController
+@RequestMapping("druid")
+public class DruidStatController {
+    @GetMapping("/stat")
+    public Object druidStat() {
+        // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据，除此之外 DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
+        return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+    }
+}
