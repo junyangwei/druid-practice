cae546f9884a4525834f6cb62c7d4f143dc8e3ca
junyangwei <799004508@qq.com>
2021-11-14 12:19:43 +0800

    feat: 使用 shardingsphere-jdbc 实现无侵入式读写分离练习

--- src/main/java/practice/druidpractice/DruidPracticeApplication.java
+++ src/main/java/practice/druidpractice/DruidPracticeApplication.java
@@ -1,10 +1,15 @@
 package practice.druidpractice;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * 启动器
+ * 	- 使用 shardingsphere-jdbc 来手动构造数据源，因此关闭 Druid 自动配置
+ */
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @MapperScan("practice.druidpractice.mapper")
 public class DruidPracticeApplication {
 
